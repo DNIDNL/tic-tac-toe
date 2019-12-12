cfecad4ec71d3d71e049b1ee08e4ed2485b2d48e
Daniel Dani <daniel.dani@ica.se>
2019-12-12 09:25:28 +0100

    Install react-router

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,36 +5,38 @@ import "./app.css"
 import styles from "./app.module.scss"
 
 const App = (props: any) => {
-   const { game, click, jump } = useGame()
+    const { game, click, jump } = useGame()
 
-   const current = game.history[game.stepNumber]
+    const current = game.history[game.stepNumber]
 
-   const winner = calculateWinner(current.squares)
+    const winner = calculateWinner(current.squares)
 
-   const moves = game.history.map((step, move) => {
-      const desc = move ? "Go to move #" + move : "Go to game start"
-      return (
-         <li key={move}>
-            <button className={styles.history} onClick={() => jump(move)}>
-               {desc}
-            </button>
-         </li>
-      )
-   })
+    const moves = game.history.map((step, move) => {
+        const desc = move ? "Go to move #" + move : "Go to game start"
+        return (
+            <li key={move}>
+                <button className={styles.history} onClick={() => jump(move)}>
+                    {desc}
+                </button>
+            </li>
+        )
+    })
 
-   const status = winner ? "Winner: " + winner : `Next player: ${game.xIsNext ? "X" : "O"}`
+    const status = winner
+        ? "Winner: " + winner
+        : `Next player: ${game.xIsNext ? "X" : "O"}`
 
-   return (
-      <div className={styles.game}>
-         <div className='game-board'>
-            <Board squares={current.squares} onClick={click} />
-         </div>
-         <div className={styles.gameInfo}>
-            <div>{status}</div>
-            <ol>{moves}</ol>
-         </div>
-      </div>
-   )
+    return (
+        <div className={styles.game}>
+            <div className="game-board">
+                <Board squares={current.squares} onClick={click} />
+            </div>
+            <div className={styles.gameInfo}>
+                <div>{status}</div>
+                <ol>{moves}</ol>
+            </div>
+        </div>
+    )
 }
 
 export default App
